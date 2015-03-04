99075ca22bec9350d35e2d098f5f8239a8548887
Christian Oien <coien@cisco.com>
2015-03-04 13:48:36 +0100

    Have trace.c compilable as C++

--- graphics/ray/trace.c
+++ graphics/ray/trace.c
@@ -11,7 +11,7 @@
 
 typedef struct {
     color lens;
-    ray ray;
+    ray ray_;
     int hop;
     bitarray * inside;
 } detector;
@@ -21,13 +21,9 @@ static color ray_trace(const detector *, world *);
     color
 trace(ray t, world * w)
 {
-    detector detector_ = {
-        {1, 1, 1},
-        .ray = t,
-        .hop = max_hops
-    };
     const size_t q = w->scene_.object_count;
-    detector_.inside = calloc(1, ba_size(q));
+    detector detector_ = { {1, 1, 1}, t, max_hops,
+        (bitarray *)calloc(1, ba_size(q)) };
     detector_.inside->bit_count = q;
     init_inside(detector_.inside, w->scene_, &t);
     if (debug && ba_firstset(detector_.inside) >= 0)
@@ -50,7 +46,7 @@ trace_hop(ray t, compact_color filter_,
         const detector * detector_, world * w)
 {
     detector t_detector = *detector_;
-    t_detector.ray = t;
+    t_detector.ray_ = t;
     t_detector.hop --;
     filter(&t_detector.lens, filter_);
     color detected = ray_trace(&t_detector, w);
@@ -105,6 +101,8 @@ refraction_trace(ray ray_, const scene_object * so,
     int outside_i = ba_firstset(detector_->inside);
     const bool enters = (outside_i != i);
     ba_assign(detector_->inside, i, enters);
+#define RETURN { ba_assign(detector_->inside, i, ( ! enters)); \
+                 return detected; }
     if ( ! enters) outside_i = ba_firstset(detector_->inside);
     real outside_refraction_index = 1;
     if (outside_i >= 0)
@@ -115,7 +113,7 @@ refraction_trace(ray ray_, const scene_object * so,
             if (detector_->hop != max_hops) /* (view _can_ happen to be inside) */
                 fprintf(stderr, "we got inside opaque object %d\n", (int)i);
         }
-        goto out;
+        RETURN
     }
     real refraction_index = outside_refraction_index
             * 1000000 /(real) so->optics.refraction_index_micro;
@@ -123,10 +121,10 @@ refraction_trace(ray ray_, const scene_object * so,
         refraction_index = 1 / refraction_index;
         scale(&ray_.head, -1);
     }
-    ray_.head = refraction(ray_.head, detector_->ray.head,
+    ray_.head = refraction(ray_.head, detector_->ray_.head,
             refraction_index, TINY_REAL);
     if (is_DISORIENTED(&ray_.head)) {
-        goto out;
+        RETURN
     }
     if (verbose) fprintf(stderr, "refraction\n");
     compact_color refraction_filter = {255, 255, 255};
@@ -134,9 +132,8 @@ refraction_trace(ray ray_, const scene_object * so,
             || ! is_near(refraction_index, 1))
         refraction_filter = so->optics.refraction_filter;
     detected = trace_hop(ray_, refraction_filter, detector_, w);
-out:
-    ba_assign(detector_->inside, i, ( ! enters));
-    return detected;
+    RETURN
+#undef RETURN
 }
 
     static color
@@ -145,7 +142,7 @@ ray_trace(const detector * detector_, world * w)
     if (0 == detector_->hop || ignorable_color(detector_->lens))
         return (color){0, 0, 0};
 
-    ray surface = detector_->ray;
+    ray surface = detector_->ray_;
     assert(is_near(length(surface.head), 1));
     stack toggled = EMPTY_STACK;
     const int detector_inside_i = ba_firstset(detector_->inside);
@@ -165,7 +162,7 @@ ray_trace(const detector * detector_, world * w)
         color ret = DIRECT_SKY;
         if ( ! eliminate_direct_sky
                 || detector_->hop != max_hops)
-            ret = w->sky(detector_->ray.head);
+            ret = w->sky(detector_->ray_.head);
         if (rgb_clear) {
             if (rgb_clear & 0x1) ret.r = 0;
             if (rgb_clear & 0x2) ret.g = 0;
@@ -176,7 +173,7 @@ ray_trace(const detector * detector_, world * w)
         const ptrdiff_t i = closest_object - w->scene_.objects;
         assert(i >= 0 && i < w->scene_.object_count);
         const bool exits = 0 < scalar_product(
-                surface.head, detector_->ray.head);
+                surface.head, detector_->ray_.head);
         const object_optics * optics = &closest_object->optics;
         object_optics auto_store;
         if (closest_object->decoration) {
@@ -197,9 +194,8 @@ ray_trace(const detector * detector_, world * w)
                 filter_.r *= reflection_on_inside;
                 filter_.g *= reflection_on_inside;
                 filter_.b *= reflection_on_inside;
-                const ray in_reflection_ = {
-                    .endpoint = surface.endpoint,
-                    .head = reflection(normal_, detector_->ray.head) };
+                const ray in_reflection_ = { surface.endpoint,
+                    reflection(normal_, detector_->ray_.head) };
                 if (verbose) fprintf(stderr, "reflection (from inside)\n");
                 detected = trace_hop(in_reflection_, filter_, detector_, w);
             }
@@ -207,9 +203,8 @@ ray_trace(const detector * detector_, world * w)
             if (debug && ba_isset(detector_->inside, i))
                 fprintf(stderr, "hit objects[%d] from outside of surface"
                         ", but book-keeping says inside of it\n", (int)i);
-            const ray reflection_ = {
-                .endpoint = surface.endpoint,
-                .head = reflection(surface.head, detector_->ray.head) };
+            const ray reflection_ = { surface.endpoint,
+                reflection(surface.head, detector_->ray_.head) };
             if (verbose) fprintf(stderr, "reflection\n");
             detected = trace_hop(
                     reflection_, optics->reflection_filter,
@@ -231,7 +226,7 @@ ray_trace(const detector * detector_, world * w)
             scene_object * io = w->scene_.objects + detector_inside_i;
             passthrough_filter(
                     &detected, &io->optics,
-                    distance(detector_->ray.endpoint, surface.endpoint));
+                    distance(detector_->ray_.endpoint, surface.endpoint));
         }
         int toggled_i;
         while ((toggled_i = st_pop(&toggled)) != STACK_VOID)
