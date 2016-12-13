1d39ba0777299e3b20b65776e9e9792c743846a2
Christian Oeien <christian.oien@gmail.com>
2016-12-13 16:40:01 -0600

    render-optimize and param-tune atoms

--- graphics/py/atomic.py
+++ graphics/py/atomic.py
@@ -5,43 +5,36 @@
 from rmg.plane import XY, origo
 from rmg.color import Color, black, white
 from rmg.math_ import rnd, rnd_weighted
-from math import atan2, sin, cos, pi
+from math import hypot, atan2, sin, cos, pi
+from itertools import product
 import os, sys
 
 
-detach_r = 3
-attach_r = 2.5
-spring_r = 2.2
-spring_k = 170
-spring_f = .23
-vecorr_d = 1.1
-blend_s = 4
-spoon_u = .16
-delta_t = .02
-steps_n = 8
+def sign(k):
+    return -1 if k < 0 else 1
 
 
 def polar(f):
     return atan2(f.y, f.x), abs(f)
 
 
-def par(p, a, r):
-    return p + XY(cos(a), sin(a)) * r
+def cpos(a, r):
+    return XY(cos(a), sin(a)) * r
+
+
+def dotprod(a, b):
+    return a.x * b.x + a.y * b.y
 
 
 def bounce(r, p, v, sv):
-    # consider: instead of bounce walls, use molecules to
-    #           move the sceene as well (instead of exception)
     n = p * (1 / abs(p))
-    m = sv.x * n.x + sv.y * n.y
+    m = dotprod(sv, n)
     c = XY(-n.y, n.x)
-    s = v.x * c.x + v.y * c.y
+    s =  dotprod(v, c)
     return n * r, c * (s * 2) + n * (m * 2) - v
 
 
-def plane_partition(a, z, m, off, rows = 12, columns = 12):
-    # improve: choose row and column so that each rect s covers
-    #          distance of min_s = 10 demmed a good size
+def plane_partition(a, z, m, off, rows = 8, columns = 8):
     r = [[] for _ in range(rows * columns)]
     s = XY(m * (columns - 1), m * (rows - 1))
     z += XY(rnd(off.x / s.x), rnd(off.y / s.y))
@@ -70,15 +63,14 @@ class Atom:
 def atom_atom(a, b):
     r = b.p - a.p
     d = abs(r)
-    if not d:
-        return
+    if not d: return
     if b in a.bond:
-        s = (d - spring_r) * spring_k
-        if s > spring_f: s -= spring_f
-        elif s < -spring_f: s += spring_f
-    elif d < attach_r:
-        s = (d - attach_r) * spring_k
+        s = spring_k * (d - spring_r) \
+                + spring_f * sign(dotprod(r, b.v) - dotprod(r, a.v))
+        # note: dampening with friction, not energy-conserving
+    elif d < attach_r: s = (d - attach_r) * spring_k
     else: return
+
     ft = r * (s / d) * delta_t
     a.work(ft)
     b.work(ft * -1)
@@ -99,55 +91,53 @@ def detach_bond(a, b):
 
 
 def attachable_angle(a, z):
-    q = XY(cos(z), sin(z))
+    q = cpos(z, 1)
     s = cos(2 * pi / (1 + a.max_bond))
     for b in a.bond:
         w = b.p - a.p
-        if w.x * q.x + w.y * q.y > s * abs(w):
+        if dotprod(w, q) > s * abs(w):
             return False
     return True
 
 
 class Lab:
-    def __init__(self, r):
+    def __init__(self, r, printer):
         self.r = r
+        self.atoms = []
+        self.printer = printer
+        self.init_spoon()
+        self.blend_r = self.spoon_h
+        self.set_blend()
         self.adjust = XY(r, r)
         self.factor = .5 / r
-        self.i = -32
-        self.atoms = []
-        self.dominant = 0
-        r *= .9
-        self.spoon_r = r * .25
-        self.spoon_h = r * .75
-        self.spoon_a = 0
-        self.blend_r = self.spoon_h
-        self.blend_a = 0
 
+    def scaled(self, p):
+        return (p + self.adjust) * self.factor
+
+    def init_spoon(self):
+        self.spoon_r = self.r * .2
+        self.spoon_h = self.r * .7
+        self.spoon_a = rnd(2 * pi)
         self.spoon_p = origo  # note: dummy--
         self.spoon_move()  # note: initialize
 
+    def set_blend(self):
+        self.dominant = int(rnd(6))
+        self.blend_a = rnd(pi * 2)
+
     def inject(self):
-        if self.i % 256 == 0:
-            self.dominant += 1
-            self.dominant %= 6
-            self.blend_a = rnd(pi * 2)
-        if self.i % 2 == 0:
-            w = [1] * 6
+        if self.printer.i % 256 == 0:
+            self.set_blend()
+        if self.printer.i % 5 == 0:
+            w = [1] * len(atoms_c)
             w[self.dominant] = 11
-            a = [   lambda p: Atom(1, 1, p),
-                    lambda p: Atom(2, 1, p),
-                    lambda p: Atom(3, 2, p),
-                    lambda p: Atom(4, 2, p),
-                    lambda p: Atom(5, 3, p),
-                    lambda p: Atom(6, 3, p)]
-            q = rnd(pi * 2)
-            p = XY(cos(q), sin(q)) * rnd(self.blend_r)
-            self.atoms.append(rnd_weighted(a, w)(p))
+            p = cpos(rnd(pi * 2), rnd(self.blend_r))
+            self.atoms.append(rnd_weighted(atoms_c, w)(p))
             self.atoms.sort(key = lambda e: e.p.y)
 
     def spoon_move(self):
         self.spoon_a += spoon_u * delta_t
-        so = XY(cos(self.spoon_a), sin(self.spoon_a)) * self.spoon_h
+        so = cpos(self.spoon_a, self.spoon_h)
         v = so - self.spoon_p
         self.spoon_p = so
         return v * (1 / delta_t)
@@ -179,22 +169,22 @@ class Lab:
                     a.p = self.spoon_p + sp
 
             if d < self.blend_r:
-                b = self.blend_a
-                a.v += XY(cos(b), sin(b)) * (1 - d / self.blend_r) \
-                        * a.m_inv * delta_t * blend_s
+                a.v += cpos(self.blend_a,
+                        (1 - d / self.blend_r) * a.m_inv * delta_t * blend_s)
 
-    def accelerate(self):
+    def work(self):
+        self.intrinsics()
         off = XY(rnd(1), rnd(1))
         for rect in plane_partition(self.atoms, XY(-self.r, -self.r), .5 / self.r, off):
-            for i, a in enumerate(rect):
+            for i, a in enumerate(rect[:-1]):
                 for b in rect[i+1:]:
                     atom_atom(a, b)
+                if abs(a.v) * delta_t * steps_n > vecorr_d:
+                    # note: error-corrections are not energy-conserving
+                    a.v *= vecorr_d / (abs(a.v) * delta_t * steps_n)
 
     def displace(self):
         for a in self.atoms:
-            if abs(a.v) * delta_t * steps_n > vecorr_d:
-                # note: error-corrections are not energy-conserving
-                a.v *= vecorr_d / (abs(a.v) * delta_t * steps_n)
             a.p += a.v * delta_t
 
     flop = False
@@ -215,79 +205,35 @@ class Lab:
                                     attachable_angle(b, z + pi):
                                 attach_bond(a, b)
 
-    def scaled(self, p):
-        return (p + self.adjust) * self.factor
-
-    def output(self):
-        self.i += 1
-        if self.i < 0:
-            return
-
-        if self.i % 16 == 0:
+    def output(self, frame):
+        if self.printer.i % 16 == 0:
             self.atoms.sort(key = lambda e: e.p.y)
             # note: maintain roughly sorted to keep less than
-            #       stripe-height difference when puts output
+            #       stripe-height difference when puts printer
+        sys.stderr.write("%d\r" % (self.printer.i,))
 
-        name = "%d.jpeg" % (self.i,)
-        s = Scanner(256, 256, name)
         for a in self.atoms:
             p = self.scaled(a.p)
-            s.put_ball(p, a.max_bond)
+            frame.put_ball(p, a.max_bond)
             for b in a.bond:
                 if id(b) < id(a):
                     q = self.scaled(b.p)
-                    s.put_dot((p + q) * .5)
+                    v = q - p
+                    for z in (.3, .5, .7):
+                        frame.put_dot(p + v * z)
 
-        s.close()
-        sys.stderr.write("%d\r" % (self.i,))
-
-    def loop(self):
+    def step(self):
         lab.inject()
-        for _q in range(steps_n):
-            lab.intrinsics()
-            lab.accelerate()
-            lab.displace()
+
         lab.restruct()
-        lab.output()
+        for _q in range(steps_n):
+            lab.work()
+            lab.displace()
 
-
-class Scanner:
-    def __init__(self, width, height, name):
-        f = os.popen("pnmtojpeg >" + name, "w")
-        s = "P6\n%d %d 255\n" % (width, height)
-        f.buffer.write(bytes(s, 'ascii'))
-        self.dim = XY(width, height)
-        stripe_h = 32
-        assert height % stripe_h == 0
-        self.stripe = Stripe(width, stripe_h, f)
-
-
-    def cell(self, p):
-        p *= self.dim
-        i, j = int(p.y), int(p.x)
-        return i, j
-
-    def put_dot(self, p):
-        i, j = self.cell(p)
-        i -= self.stripe.prepare(i)
-        self.stripe.put(i, j, white)
-
-    sat = [1] * 4 + [.5] + [1] * 4
-    val = [.5, 1, .5, 1, 1, 1, .5, 1, .5]
-    def put_ball(self, p, hue_i):
-        assert hue_i
-        i, j = self.cell(p)
-        i -= self.stripe.prepare(i + 1)  # quantity: +1 for centered 3x3 below
-        p_i = sum(([k] * 3 for k in (i - 1, i, i + 1)), [])
-        p_j = [j - 1, j, j + 1] * 3
-        for q in range(9):
-            c = Color.from_hsv(hue_i * pi/3, Scanner.sat[q], Scanner.val[q])
-            self.stripe.put(p_i[q], p_j[q], c)
-
-    def close(self):
-        for _ in range(self.dim.y - self.stripe.offset):
-            self.stripe.flush()
-        self.stripe.f.close()
+        frame = self.printer.frame()
+        if frame:
+            lab.output(frame)
+            frame.close()
 
 
 class Stripe:
@@ -318,9 +264,95 @@ class Stripe:
         if i < 0 or i >= self.height or j < 0 or j >= self.width:
             return
 
-        self.b[i][j] = c
+        w = self.b[i][j]
+        w += c
+        w.cap()
+        self.b[i][j] = w
 
 
-lab = Lab(60)
-for _i in range(2024):
-    lab.loop()
+class Frame:
+    def __init__(self,  width, height, name, ball):
+        f = os.popen("pnmtojpeg >" + name, "w")
+        s = "P6\n%d %d 255\n" % (width, height)
+        f.buffer.write(bytes(s, 'ascii'))
+        self.dim = XY(width, height)
+        stripe_h = 64
+        assert height % stripe_h == 0
+        self.stripe = Stripe(width, stripe_h, f)
+        self.ball = ball
+
+    def cell(self, p):
+        p *= self.dim
+        i, j = int(p.y), int(p.x)
+        return i, j
+
+    def put_dot(self, p):
+        i, j = self.cell(p)
+        i -= self.stripe.prepare(i)
+        self.stripe.put(i, j, white)
+
+    def put_ball(self, p, hue_i):
+        assert hue_i
+        i, j = self.cell(p)
+        i -= self.stripe.prepare(i + self.ball.r)
+        for q, (y, x) in enumerate(self.ball.offsets):
+            c = Color.from_hsv(hue_i * pi/3,
+                    self.ball.saturations[q],
+                    self.ball.values[q])
+            self.stripe.put(i + y, j + x, c)
+
+    def close(self):
+        for _ in range(self.dim.y - self.stripe.offset):
+            self.stripe.flush()
+        self.stripe.f.close()
+
+
+class Ball:
+    def __init__(self, r):
+        self.r = r
+        self.offsets = list(product(*[range(-r, r+1)]*2))
+        s, v = [], []
+        for x, y in self.offsets:
+            s.append(min(1, (.5+hypot(x, y)/r)))
+            v.append(max(0, min(1, 2-2*hypot(x, y)/r)))
+        self.saturations = s
+        self.values = v
+
+
+class Printer:
+    def __init__(self, width, height, ball_r, name_fmt, skip_i):
+        self.width = width
+        self.height = height
+        self.name_fmt = name_fmt
+        self.i = -skip_i
+        self.ball = Ball(ball_r)
+
+    def frame(self):
+        i = self.i
+        self.i += 1
+        if i < 0: return
+
+        name = self.name_fmt % (i,)
+        return Frame(self.width, self.height, name, self.ball)
+
+
+atoms_c = [
+        lambda p: Atom(1, 1, p),
+        lambda p: Atom(2, 1, p),
+        lambda p: Atom(3, 2, p),
+        lambda p: Atom(4, 2, p),
+        lambda p: Atom(5, 3, p),
+        lambda p: Atom(6, 3, p)]
+detach_r = 3
+attach_r = 2.4
+spring_r = 2.2
+spring_k = 560
+spring_f = 4.2
+vecorr_d = 1.1
+delta_t = .008
+blend_s = 4.2
+spoon_u = .24
+steps_n = 4
+lab = Lab(50, Printer(512, 512, 3, "%d.jpeg", 100))
+for _ in range(2024):
+    lab.step()
