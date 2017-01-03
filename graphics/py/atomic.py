c63a9f721a16bb480c8ff8cb1ad891897d2637d0
Christian Oeien <christian.oien@gmail.com>
2017-01-02 20:27:40 -0600

    Tuning of atomic parameters
    Also have figures be enlightened by atoms

--- graphics/py/atomic.py
+++ graphics/py/atomic.py
@@ -66,11 +66,11 @@ def detach_bond(a, b):
 
 
 class PhysPars:
-    def __init__(self, a_r, s_k, s_f):
-        self.vecorr_d = a_r
+    def __init__(self, s_r, d_r, s_k, s_f):
+        self.vecorr_d = a_r = 1
         self.attach_r = a_r * 2
-        self.spring_r = a_r * 1.83
-        self.detach_r = a_r * 2.42
+        self.spring_r = s_r * 2
+        self.detach_r = d_r * 2
         self.spring_f = s_f
         self.spring_k = s_k
         self.bounce_c = s_k * .5
@@ -91,34 +91,42 @@ class PhysPars:
 
 
 class Blend:
-    def __init__(self, h, w, s):
+    def __init__(self, h, w, q, s):
         self.h = h
         self.w = w
+        self.q = q
         self.s = s
         self.atoms_c = [
                 lambda p: Atom(1, 2, p),
-                lambda p: Atom(2, 3, p),
+                lambda p: Atom(2, 2, p),
                 lambda p: Atom(3, 3, p),
-                lambda p: Atom(4, 4, p),
+                lambda p: Atom(4, 3, p),
                 lambda p: Atom(5, 4, p),
-                lambda p: Atom(6, 5, p)]
-        self.reset()
+                lambda p: Atom(6, 4, p)]
 
-    def reset(self):
-        self.dominant = int(rnd(len(self.atoms_c)))
-        self.a = rnd(pi * 2)
+    def create(self, n):
+        nc = len(self.atoms_c)
+        if n >= self.q * nc: return
 
-    def create(self):
-        w = [1] * len(self.atoms_c)
-        w[self.dominant] = 11
+        c = nc - 1 - n // self.q
+        w = [1] * nc
+        w[c] = (nc - 1) * 7
         p = cpos(rnd(pi * 2), rnd(self.h))
         return rnd_weighted(self.atoms_c, w)(p)
 
     def effect(self, a, delta_t):
         d = abs(a.p)
         if d < self.w:
-            a.v += cpos(self.a,
-                    (1 - d / self.w) * a.m_inv * delta_t * self.s)
+            a.v.y += (1 - d / self.w) * a.m_inv * delta_t * self.s
+
+
+def castm(p, atoms):
+    m = 0
+    for c in atoms:
+        r = cast_r - abs(c.p - p)
+        if r >= 0:
+            m += r / cast_r
+    return m * shade_m
 
 
 class Spoon:
@@ -135,19 +143,28 @@ class Spoon:
         self.r = .25 * (3 + sin(t * self.w)) * self.m
 
     def effect(self, a, delta_t):
-        d = abs(a.p)
-        if abs(d - self.h) < self.r:
-            sp = a.p - self.p
-            sd = abs(sp)
-            if sd < self.r:
-                n = sp * (1 / sd)
-                a.v += n * delta_t * self.s * (self.r - sd)
+        sp = a.p - self.p
+        sd = abs(sp)
+        if sd < self.r:
+            n = sp * (1 / sd)
+            a.v += n * delta_t * self.s * (self.r - sd)
+
+    def set_casters(self, atoms):
+        c = self.casters = []
+        # optim: bisect for a.p.y closest to but grater than p.y - r - cast_r
+        #        and (to end at) the one closest but less than p.y + r + cast_r
+        #        since we know atoms sorted on a.p.y we can loop over only these
+        for a in atoms:
+            if abs(a.p - self.p) < self.r + cast_r:
+                c.append(a)
 
     def _overlay_shade(self, color, p):
         if p.x > self.p.x - self.r and p.x < self.p.x + self.r:
             d = self.r - abs(p - self.p)
             if d >= 0 and d < self.b:
-                return color.mix(white, shade_m)
+                m = castm(p, self.casters)
+                if m >= 1: return white
+                elif m: return color.mix(white, m)
         return color
 
     def overlay_f(self, y):
@@ -208,6 +225,22 @@ class Fork:
             if self._leftside(a.p): self._effect(a, self.pl, delta_t)
             elif self._rightside(a.p): self._effect(a, self.pr, delta_t)
 
+    def _caster_z(self, a_z, p_z, h_z):
+        return a_z + cast_r > p_z and a_z - cast_r < p_z + h_z
+
+    def set_casters(self, atoms):
+        c = self.casters = []
+        # optim: bisect for a.p.y closest to pl.y - cast_r
+        #        and (to end at) pl.y + h.y + cast_r
+        #        since we know atoms sorted on a.p.y
+        #        we can loop over these and thus skip the
+        #        first of following _caster_z calls
+        for a in atoms:
+            if self._caster_z(a.p.y, self.pl.y, self.h.y) and (
+                    self._caster_z(a.p.x, self.pl.x, self.h.x)
+                    or self._caster_z(a.p.x, self.pr.x, self.h.x)):
+                c.append(a)
+
     def _overlay_shade(self, color, ip):
         if self._outside_x(ip.x): return color
         if self._leftside(ip): p = self.pl
@@ -215,7 +248,9 @@ class Fork:
         else: return color
         if ip.x - p.x < self.b or p.x + self.h.x - ip.x < self.b \
                 or ip.y - p.y < self.b or p.y + self.h.y - ip.y < self.b:
-            return color.mix(white, shade_m)
+            m = castm(ip, self.casters)
+            if m >= 1: return white
+            elif m: return color.mix(white, m)
         return color
 
     def overlay_f(self, y):
@@ -232,7 +267,7 @@ class Hue6Shift:
         self.q = cpos(self.u * t, self.r_inv)
 
     def __call__(self, p):
-        return dotprod(p, self.q)
+        return dotprod(p, self.q) - .6
 
 
 class Bulb:
@@ -244,7 +279,7 @@ class Bulb:
             d = hypot(x, y)
             if not d: v = 1
             elif d > r: v = 0
-            else: v = 1 - (d / r)
+            else: v = (1 - (d / r)) ** 1.83
             a.append(v)
         self.values = a
 
@@ -266,7 +301,8 @@ class Lab:
         self.par = par
         self.delta_t = delta_t
         self.corner = dim * .5
-        self.bulb = Bulb(int(1.3 * printer.height / dim.y))
+        self.bulb = Bulb(int(par.spring_r * .5 * printer.height / dim.y - .5))
+        self.line_n = int(par.spring_r * printer.height / dim.y)
         self.atoms = []
         self.blend = blend
         self.spoon = spoon
@@ -280,18 +316,20 @@ class Lab:
         return (p + self.corner) * self.zoom
 
     def _inject(self):
-        if self.printer.i % 256 == 0:
-            self.blend.reset()
-        if self.printer.i % 3 == 0:
-            self.atoms.append(self.blend.create())
+        a = self.blend.create(len(self.atoms))
+        if a:
+            self.atoms.append(a)
             self.atoms.sort(key = lambda e: e.p.y)
+
+    def _border_force(self, d):
+        return d * self.delta_t * self.par.bounce_c
 
     def _border(self, a):
         p, v, r = a.p, a.v, self.corner
-        if p.x  < - r.x: p.x, v.x = -r.x, -v.x
-        elif p.x >= r.x: p.x, v.x = +r.x, -v.x
-        if p.y  < - r.y: p.y, v.y = -r.y, -v.y
-        elif p.y >= r.y: p.y, v.y = +r.y, -v.y
+        if p.x  < - r.x + 1: v.x += self._border_force((-r.x + 1) - p.x)
+        elif p.x >= r.x - 1: v.x -= self._border_force(p.x - (r.x - 1))
+        if p.y  < - r.y + 1: v.y += self._border_force((-r.y + 1) - p.y)
+        elif p.y >= r.y - 1: v.y -= self._border_force(p.y - (r.y - 1))
 
     def _intrinsics(self):
         self.fork.repos(self.delta_t)
@@ -335,26 +373,18 @@ class Lab:
                                 attach_bond(a, b)
 
     def _output(self, frame):
-        if self.printer.i % 4 == 0:
-            self.atoms.sort(key = lambda e: e.p.y)
-            # note: maintain roughly sorted to keep less than
-            #       roll-height difference when printer dumps
-        sys.stderr.write("#%d@" % (self.printer.i,))
-        sys.stderr.flush()
         self.hue6shift.update(self.t)
+        self.atoms.sort(key = lambda e: e.p.y)
         for a in self.atoms:
-            hue6 = a.max_bond + self.hue6shift(a.p)
+            shift = self.hue6shift(a.p)
+            hue6 = shift - a.max_bond
             p = self._scaled(a.p)
             frame.put_bulb(p, hue6, self.bulb)
             for b in a.bond:
                 if id(b) < id(a):
                     q = self._scaled(b.p)
-                    frame.put_line(p, q, 3 * self.bulb.r)
-
-        realt = time()
-        calct = realt - self.realt
-        self.realt = realt
-        sys.stderr.write("%.2fs in %.2fs\r" % (self.t, calct))
+                    h6q = shift - b.max_bond
+                    frame.put_line(p, q, hue6, h6q, self.line_n)
 
     def run(self, n):
         overlay = Overlay(lambda i, j:
@@ -371,10 +401,19 @@ class Lab:
                 lab._work()
                 lab._displace()
 
+            sys.stderr.write("#%d@" % (self.printer.i,))
+            sys.stderr.flush()
             frame = self.printer.frame(overlay)
             if frame:
+                self.spoon.set_casters(self.atoms)
+                self.fork.set_casters(self.atoms)
                 lab._output(frame)
                 frame.close()
+            realt = time()
+            calct = realt - self.realt
+            self.realt = realt
+            sys.stderr.write("%.2fs in %.2fs\r" % (self.t, calct))
+
 
 
 class Overlay:
@@ -398,46 +437,39 @@ class Overlay:
         return color
 
 
-class Writer:
-    def __init__(self, outfile, overlay):
-        self.outfile = outfile
-        self.overlay = overlay
-
-    def put(self, i, line):
-        b = self.outfile.buffer
-        s = self.overlay.line_shaders(i)
-        if not s:
-            for c in line:
-                b.write(c.binary_rgb())
-        else:
-            for j, c in enumerate(line):
-                b.write(self.overlay.shade(c, i, j, s).binary_rgb())
-
-
-class Roll:
-    def __init__(self, width, height, writer):
+class RollWriter:
+    def __init__(self, width, height, outfile, overlay):
         self.width = width
         self.height = height
         self.offset = 0
-        self.writer = writer
         self.lines = [[black] * width for _ in range(height)]
+        self.outfile = outfile
+        self.overlay = overlay
 
     def advance(self, i):
         i -= self.offset
         r = self.offset
         while i >= self.height:
-            self.flush_line()
+            self.write_line()
             i -= 1
             r += 1
         return r
 
-    def flush_line(self):
-        self.writer.put(self.offset, self.lines[0])
+    def write_line(self):
+        i = self.offset
+        b = self.outfile.buffer
+        s = self.overlay.line_shaders(i)
+        if not s:
+            for c in self.lines[0]:
+                b.write(c.binary_rgb())
+        else:
+            for j, c in enumerate(self.lines[0]):
+                b.write(self.overlay.shade(c, i, j, s).binary_rgb())
         self.offset += 1
         del self.lines[0]
         self.lines.append([black] * self.width)
 
-    def set_el(self, i, j, c):
+    def add_el(self, i, j, c):
         if i < 0 or i >= self.height or j < 0 or j >= self.width:
             return
 
@@ -448,8 +480,8 @@ class Roll:
 
     def close(self, dim_height):
         for _ in range(dim_height - self.offset):
-            self.flush_line()
-        self.writer.outfile.close()
+            self.write_line()
+        self.outfile.close()
 
 
 class Frame:
@@ -460,31 +492,41 @@ class Frame:
         self.dim = XY(width, height)
         roll_h = height // 20
         assert height % roll_h == 0
-        self.roll = Roll(width, roll_h, Writer(f, overlay))
+        self.roll = RollWriter(width, roll_h, f, overlay)
 
     def _pixel(self, p):
         p *= self.dim
         i, j = int(p.y), int(p.x)
+        if i < 0: i = 0
+        elif i >= self.dim.y:
+            i = self.dim.y - 1
+        if j < 0: j = 0
+        elif j >= self.dim.x:
+            j = self.dim.x - 1
         return i, j
 
-    def put_line(self, p, q, n):
-        i, _ = self._pixel(p)
-        self.roll.advance(i)
-        i, _ = self._pixel(q)
-        self.roll.advance(i)
+    def put_line(self, p, q, h6p, h6q, n):
+        c = Color.from_hsv(h6p * pi/3, 1, line_v)
+        if h6p == h6q: cstp = None
+        else:
+            cq = Color.from_hsv(h6q * pi/3, 1, line_v)
+            cstp = (cq - c) * (1 / (n - 2))
+        self.roll.advance(self._pixel(p)[0])
+        self.roll.advance(self._pixel(q)[0])
         step = (q - p) * (1 / n)
         for z in range(1, n - 1):
             p += step
+            if cstp: c += cstp
             i, j = self._pixel(p)
             i -= self.roll.offset
-            self.roll.set_el(i, j, linep_c)
+            self.roll.add_el(i, j, c)
 
     def put_bulb(self, p, hue6, bulb):
         i, j = self._pixel(p)
         i -= self.roll.advance(i + bulb.r)
         for q, (y, x) in enumerate(bulb.offsets):
             c = Color.from_hsv(hue6 * pi/3, 1, bulb.values[q])
-            self.roll.set_el(i + y, j + x, c)
+            self.roll.add_el(i + y, j + x, c)
 
     def close(self):
         self.roll.close(self.dim.y)
@@ -506,17 +548,16 @@ class Printer:
         return Frame(self.width, self.height, overlay, name)
 
 
-_d = XY(1280, 720)
 _d = XY(640, 360)
-_h, _ar = 100, _d.x / _d.y
-linep_c = white * .15
-shade_m = .1
-intro_n = 12
-par = PhysPars(1.2, 500, 3.9)
-lab = Lab(XY(_h * _ar, _h), par, .017,
-        Blend(_h * .1, _h * .3, 6),
-        Spoon(.9, _h * .35, .5, _h * .15, par.bounce_c, 2.4),
-        Fork(XY(_ar, 1) * _h * .45, XY(.4, .1) * _h, 7, par.bounce_c, 2.4),
-        Hue6Shift(_h * _ar, .8),
-        Printer(_d, "%d.jpeg", intro_n))
+_h, _ar = 86, _d.x / _d.y
+line_v = .3958
+shade_m = .046
+cast_r = 5.512
+intro_n = 123
+par = PhysPars(1.19, 1.67, 475, 4.09)
+lab = Lab(XY(_h * _ar, _h), par, .0148,
+        Blend(_h * .16, _h * .26, 142, 6.3),
+        Spoon(.55, _h * .35, .4, _h * .15, par.bounce_c, 1),
+        Fork(XY(_ar, 1) * _h * .48, XY(.19416, .12) * _h, 4.9, par.bounce_c, 1),
+        Hue6Shift(_h * _ar * 1.19, .8), Printer(_d, "%d.jpeg", intro_n))
 lab.run(4096)
