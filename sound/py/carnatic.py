a484922d2b4c9a2364351e33c3d7e00c01e907a8
Christian Oeien <christian.oien@gmail.com>
2019-01-28 23:23:37 -0600

    sound improved for carnatic

--- sound/py/carnatic.py
+++ sound/py/carnatic.py
@@ -51,23 +51,50 @@ def swara_frequency(swara, r, adhara):
 
 def sarali(n, gati):
     assert gati == 1
-    b = randrange(-8, 8)
+    b = randrange(0, 8)
     a = list(range(b, b + n))
     if choice([0, 1]):
         a.reverse()
     return a[:n]
 
-
+# usage: aplay -f S16_BE -r 40100 out.raw
 if __name__ == "__main__":
     from math import sin, cos, pi
-    sr = 8000
-    with open("out.U8", "wb") as o:
-        for c in range(8):
+    def out(v):
+        u = int(32767 * v)
+        if u < 0: u += 65536
+        o.write(bytes([u >> 8, u & 255]))
+    sr = 40100
+    speed = .7
+    def nsamp(t):
+        return int(speed * sr / t)
+    def pause(t):
+        for i in range(nsamp(t)):
+            out(0)
+    def note(t, s, amp):
+        n = nsamp(t)
+        f = swara_frequency(s, mayamalavagowla, 400)
+        fmx = randrange(2, 4)
+        fma = randrange(2, 4)
+        att = int(sr // f) * randrange(1, 5)
+        m = n - att
+        for i in range(n):
+            if i < att: a = amp * i / att
+            else: a = amp * (n - i) / m
+            x = i * f / sr
+            out(a * sin(x + fma * a * sin(x * fmx)))
+    aa, ab, ac = .78, .71, .65
+    with open("out.raw", "wb") as o:
+        for i, c in enumerate(range(64)):
             for s in compose("IOO", 4, 1, sarali):
-                f = swara_frequency(s, mayamalavagowla, 200)
-                n = sr // 4
-                for i in range(n):
-                    a = 47 * (1 - cos(pi * 2 * (n - i) / n))
-                    v = 127 + int(a * sin(pi * 2 * i * f / sr))
-                    o.write(bytes((v,)))
-
+                alt = randrange(36)
+                if alt == 0:
+                    note(8, s, ab)
+                    note(8, s + 1, ac)
+                elif alt == 1:
+                    note(8, s + 1, ab)
+                    note(8, s, ac)
+                elif alt < 5 and i % 4 != 0:
+                    pause(4)
+                else:
+                    note(4, s, aa if i % 8 == 0 else ab)
