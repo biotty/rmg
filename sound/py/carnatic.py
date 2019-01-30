ae01f359d47aa24276754bed2bf1495bc591a75a
Christian Oeien <christian.oien@gmail.com>
2019-01-29 22:57:15 -0600

    command args to carnatic

--- sound/py/carnatic.py
+++ sound/py/carnatic.py
@@ -23,8 +23,6 @@ def chakra():
 melakarta = [raga(r, g, m, d, n) for m in range(2)
         for r, g in chakra() for d, n in chakra()]
 
-mayamalavagowla = melakarta[14]
-
 def abrev(n):
     c = "srgmpdn"[n % 7]
     o = n // 7
@@ -60,41 +58,65 @@ def sarali(n, gati):
 # usage: aplay -f S16_BE -r 40100 out.raw
 if __name__ == "__main__":
     from math import sin, cos, pi
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument(
+            "-a", "--adhara", type=int,
+            help="base frequency", default=110)
+    parser.add_argument(
+            "-r", "--raga", type=int,
+            help="melakarta index", default=14)
+    args = parser.parse_args()
+    our_adhara = args.adhara
+    our_raga = melakarta[args.raga]
+
+    def sine(x):
+        return sin(2 * pi * x)
     def out(v):
         u = int(32767 * v)
         if u < 0: u += 65536
         o.write(bytes([u >> 8, u & 255]))
     sr = 40100
-    speed = .7
+    speed = .8
     def nsamp(t):
         return int(speed * sr / t)
-    def pause(t):
-        for i in range(nsamp(t)):
-            out(0)
-    def note(t, s, amp):
-        n = nsamp(t)
-        f = swara_frequency(s, mayamalavagowla, 400)
-        fmx = randrange(2, 4)
-        fma = randrange(2, 4)
-        att = int(sr // f) * randrange(1, 5)
+    def intra(amp):
+        n = nsamp(1 / 2)
+        f = swara_frequency(randrange(0, 2) * 2, our_raga, our_adhara)
+        att = int(sr // f)
         m = n - att
         for i in range(n):
             if i < att: a = amp * i / att
             else: a = amp * (n - i) / m
             x = i * f / sr
-            out(a * sin(x + fma * a * sin(x * fmx)))
-    aa, ab, ac = .78, .71, .65
+            out(a * sine(x * 2))
+    def pause(t):
+        for i in range(nsamp(t)):
+            out(0)
+    def note(t, s, amp):
+        n = nsamp(t)
+        f = swara_frequency(s, our_raga, our_adhara)
+        att = int(sr // f) * randrange(2, 7)
+        m = n - att
+        for i in range(n):
+            if i < att: a = amp * i / att
+            else: a = amp * (n - i) / m
+            x = i * f / sr
+            out(a * (sine(x) + sine(x * 4)))
+    aa, ab, ac = .5, .4, .3
     with open("out.raw", "wb") as o:
-        for i, c in enumerate(range(64)):
-            for s in compose("IOO", 4, 1, sarali):
-                alt = randrange(36)
+        for i, c in enumerate(range(8)):
+            for j, s in enumerate(compose("IOO", 4, 1, sarali)):
+                alt = randrange(40)
                 if alt == 0:
                     note(8, s, ab)
                     note(8, s + 1, ac)
                 elif alt == 1:
                     note(8, s + 1, ab)
                     note(8, s, ac)
-                elif alt < 5 and i % 4 != 0:
+                elif alt < 5 and j % 4 not in (0, 3):
                     pause(4)
                 else:
                     note(4, s, aa if i % 8 == 0 else ab)
+            if i % 4 == 3:
+                intra(ac)
