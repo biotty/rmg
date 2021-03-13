065bebc8e4c9eb15fa9687621b7f76d03b04bcea
Christian Oeien <christian.oien@gmail.com>
2021-03-12 23:29:44 -0600

    cv.wait while in the critical region
    but dont care about spurious, as not any race, so will be rare

--- graphics/ray/render.cpp
+++ graphics/ray/render.cpp
@@ -39,39 +39,38 @@ struct RasterJob {
     serial_t output_serial;
     using result = Sequenced<color>;
     std::vector<result> output_buffer;
-    const size_t max_output_buffer_size;
+    const size_t lim_output_buffer_size;
     std::function<color(type)> f;
     image out;
 
     RasterJob(int width, int height,
             std::function<color(type)> f, image out, size_t mobs)
         : input_serial(), x(), y(), w(width), h(height)
-        , output_serial(), max_output_buffer_size(mobs), f(f), out(out) {}
+        , output_serial(), lim_output_buffer_size(mobs), f(f), out(out) {}
 
-    bool output(result r) {
-        {
-            std::lock_guard<std::mutex> guard(output_mutex);
-            output_buffer.push_back(r);
-            const auto was_serial = output_serial;
-            while (true) {
-                auto it = std::find_if(output_buffer.begin(), output_buffer.end(),
-                        [this](result & r)
-                        { return output_serial == r.serial; });
-                if (it == output_buffer.end())
-                    break;
+    void output(result r) {
+        std::unique_lock<std::mutex> lk(output_mutex);
+        output_buffer.push_back(r);
+        const auto was_serial = output_serial;
+        while (true) {
+            auto it = std::find_if(output_buffer.begin(), output_buffer.end(),
+                    [this](result & r)
+                    { return output_serial == r.serial; });
+            if (it == output_buffer.end())
+                break;
 
-                image_write(out, it->value);
+            image_write(out, it->value);
 
-                output_buffer.erase(it);
-                ++output_serial;
-            }
-
-            if (output_serial == was_serial && output_buffer.size() > max_output_buffer_size) {
-                return true;
-            }
+            output_buffer.erase(it);
+            ++output_serial;
         }
-        cv.notify_all();
-        return false;
+
+        if (output_serial == was_serial && output_buffer.size() > lim_output_buffer_size) {
+            cv.wait(lk);
+        } else {
+            lk.unlock();
+            cv.notify_all();
+        }
     }
 
     Sequenced<type> input() {
@@ -91,13 +90,7 @@ struct RasterJob {
 
     void run() {
         while (auto s = input()) {
-            if (output({s.serial, f(s.value)})) {
-                std::unique_lock<std::mutex> lk(output_mutex);
-                for (const auto was_serial = output_serial;
-                        was_serial == output_serial;
-                        cv.wait(lk))
-                    ;
-            }
+            output({s.serial, f(s.value)});
         }
     }
 };
